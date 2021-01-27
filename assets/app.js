397bf9f453d3e26913abfded442ca758a06b0be9
web-2dev <ded.developpeur.web@gmail.com>
2021-01-27 18:40:20 +0100

    Fix erreur Bootstrap affichage du fichier uploadé d'un input type file

--- assets/app.js
+++ assets/app.js
@@ -11,9 +11,18 @@ import './styles/app.scss';
 // start the Stimulus application
 // import './bootstrap';
 const $ = require('jquery');
+global.$ = global.jQuery = $;
+
 require('bootstrap');
 
 
 $(function(){
-    console.log("js");
-})
+    $("[type='file']").on("change", function(){
+        var formulaire = $(this).closest("form")[0];
+        var identifiant = $(this).prop("id") ? $(this).prop("id") : $(this).prop("name");
+        var fichier = formulaire[identifiant].files[0];
+        var fileName = fichier.name;
+        $(this).next('.custom-file-label').html(fileName);
+        
+    });
+});
